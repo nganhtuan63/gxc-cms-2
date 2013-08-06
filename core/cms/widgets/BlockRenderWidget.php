327b4ca38d29ab97abbaec01b2b537796320ad02
nganhtuan63 <nganhtuan63@gmail.com>
2013-08-06 15:39:36 +0700

    Update BlockRenderWidget.php
    Disable the caching for BLock Render - which I found a little bit annoy.

--- core/cms/widgets/BlockRenderWidget.php
+++ core/cms/widgets/BlockRenderWidget.php
@@ -32,28 +32,31 @@ class BlockRenderWidget extends CWidget
  
     protected function renderContent()
     {       
-        if (isset($this->page)) {                               	
+        if (isset($this->page)) {   
+        	
+        	$connection=Yii::app()->db;
+            $command=$connection->createCommand('SELECT b.block_id,b.name,b.type,b.params FROM 
+            	{{page_block}} pb INNER JOIN {{block}} b ON
+            	pb.block_id=b.block_id  
+            	WHERE page_id=:paramId and pb.region=:regionId and pb.status=:status 
+            	order by pb.block_order ASC limit 20');
+            $command->bindValue(':paramId',$this->page['page_id'],PDO::PARAM_INT); 
+            $command->bindValue(':regionId',$this->region,PDO::PARAM_INT); 
+            $command->bindValue(':status',ConstantDefine::PAGE_BLOCK_ACTIVE,PDO::PARAM_INT); 		            
+            $blocks=$command->queryAll();          		            		            		            		            
+			if($blocks!==false){						
+				//Yii::app()->cache->set('pb-'.$this->page['page_id'].'-'.$this->region,$blocks,1800);						
+				$this->workWithBlocks($blocks);
+			} else {
+				echo '';
+			}
+			/* Disable Cache Here
 			$blocks=Yii::app()->cache->get('pb-'.$this->page['page_id'].'-'.$this->region);					
-			if($blocks===false){												   
-		            $connection=Yii::app()->db;
-		            $command=$connection->createCommand('SELECT b.block_id,b.name,b.type,b.params FROM 
-		            	{{page_block}} pb INNER JOIN {{block}} b ON
-		            	pb.block_id=b.block_id  
-		            	WHERE page_id=:paramId and pb.region=:regionId and pb.status=:status 
-		            	order by pb.block_order ASC limit 20');
-		            $command->bindValue(':paramId',$this->page['page_id'],PDO::PARAM_INT); 
-		            $command->bindValue(':regionId',$this->region,PDO::PARAM_INT); 
-		            $command->bindValue(':status',ConstantDefine::PAGE_BLOCK_ACTIVE,PDO::PARAM_INT); 		            
-		            $blocks=$command->queryAll();          		            		            		            		            
-					if($blocks!==false){						
-						Yii::app()->cache->set('pb-'.$this->page['page_id'].'-'.$this->region,$blocks,1800);						
-						$this->workWithBlocks($blocks);
-					} else {
-						echo '';
-					}
+			if($blocks===false){												   		          
 			} else {
 				$this->workWithBlocks($blocks);
-			}                            	                	   	 
+			}   
+			*/                         	                	   	 
         }       
     }   
 
